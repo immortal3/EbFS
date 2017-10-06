13152d4144550a1818e7625edf47c2d973117cef
immortal3 <dip.patel.ict@gmail.com>
2017-10-06 15:47:40 +0530

    uploading disk emulator

---
+++ Source/disk.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "disk.h"
+/*Reference : https://www3.nd.edu/~dthain/courses/cse30341/spring2017/project6/ */
+#define DISK_MAGIC 0xdeadbeef
+
+static FILE *diskfile;
+static int nblocks=0;
+static int nreads=0;
+static int nwrites=0;
+
+
+/* creating or loading file which will work as disk */
+int disk_init( const char *filename, int n )
+{
+	diskfile = fopen(filename,"r+");
+	if(!diskfile) diskfile = fopen(filename,"w+");
+	if(!diskfile) return 0;
+
+	ftruncate(fileno(diskfile),n*DISK_BLOCK_SIZE);
+
+	nblocks = n;
+	nreads = 0;
+	nwrites = 0;
+
+	return 1;
+}
+
+
+/* will return total number of block (block size  = 4kb ) */
+int disk_size()
+{
+	return nblocks;
+}
+
+
+/* checking number of bad blocks given block number   */
+static void sanity_check( int blocknum, const void *data )
+{
+	if(blocknum<0) {
+		printf("ERROR: blocknum (%d) is negative!\n",blocknum);
+		abort();
+	}
+
+	if(blocknum>=nblocks) {
+		printf("ERROR: blocknum (%d) is too big!\n",blocknum);
+		abort();
+	}
+
+	if(!data) {
+		printf("ERROR: null data pointer!\n");
+		abort();
+	}
+}
+
+
+/* disk_read will read data given block number */
+
+void disk_read( int blocknum, char *data )
+{
+	sanity_check(blocknum,data);
+
+	fseek(diskfile,blocknum*DISK_BLOCK_SIZE,SEEK_SET);
+
+	if(fread(data,DISK_BLOCK_SIZE,1,diskfile)==1) {
+		nreads++;
+	} else {
+		printf("ERROR: couldn't access simulated disk: %s\n",strerror(errno));
+		abort();
+	}
+}
+
+
+
+void disk_write( int blocknum, const char *data )
+{
+	sanity_check(blocknum,data);
+
+	fseek(diskfile,blocknum*DISK_BLOCK_SIZE,SEEK_SET);
+
+	if(fwrite(data,DISK_BLOCK_SIZE,1,diskfile)==1) {
+		nwrites++;
+	} else {
+		printf("ERROR: couldn't access simulated disk: %s\n",strerror(errno));
+		abort();
+	}
+}
+
+
+/* closing virtual disk based on file  */
+void disk_close()
+{
+	if(diskfile)
+  {
+		printf("%d disk block reads\n",nreads);
+		printf("%d disk block writes\n",nwrites);
+		fclose(diskfile);
+		diskfile = 0;
+	}
+}
---
+++ Source/disk.h
@@ -0,0 +1,19 @@
+#ifndef DISK_H
+#define DISK_H
+/*Reference : https://www3.nd.edu/~dthain/courses/cse30341/spring2017/project6/ */
+
+/* Disk block of size = 4 kb  */
+#define DISK_BLOCK_SIZE 4096
+
+
+/* defining methods
+    For methods description check .c file
+*/
+int  disk_init( const char *filename, int nblocks );
+int  disk_size();
+void disk_read( int blocknum, char *data );
+void disk_write( int blocknum, const char *data );
+void disk_close();
+
+
+#endif
