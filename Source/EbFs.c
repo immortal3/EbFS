d8a94dbd4ea0c7db0434746c0abc574f1ad3994f
Unknown <dip.patel.ict@gmail.com>
2017-10-15 19:35:10 +0530

    Code for formatting

--- Source/EbFs.c
+++ Source/EbFs.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-#include "disk.c"
-#include "inode.c"
-#include "superblock.c"
+#include "filesystem.c"
 int main()
 {
 
 	struct inode temp;
-	struct superblock s1;
 
 	// creating virtual disk of 500 blocks 
 	// total size = 500 * 4 kb
 	int disk_status = disk_init("Disk_img_500",500);
 	// Debug :: printf("Disk  number of blocks : %d\n",disk_size() );
-	char data[DISK_BLOCK_SIZE] = "Hello";
-	disk_write(25,data);
-	char bufffer[DISK_BLOCK_SIZE];
-	disk_read(25,bufffer);
+	// Debug :: char data[DISK_BLOCK_SIZE] = "Hello";
+	// Debug :: disk_write(25,data);
+ 	// Debug :: char bufffer[DISK_BLOCK_SIZE];
+	// Debug :: disk_read(25,bufffer);
 	// Debug :: printf("Readed Value : %s\n", bufffer);
 
-	printf("Size of inode : %ld byte(s)\n",sizeof(temp));
-	printf("Size of Super block : %ld byte(s)\n",sizeof(s1) );
+	// Debug :: printf("Size of inode : %ld byte(s)\n",sizeof(temp));
+	// Debug :: printf("Size of Super block : %ld byte(s)\n",sizeof(s1) );
+	EbFs_format();
+	disk_close();
 
 }
 
---
+++ Source/filesystem.h
@@ -0,0 +1,6 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+
+#define MINIMUM_DISk_SIZE 30
+
+#endif
